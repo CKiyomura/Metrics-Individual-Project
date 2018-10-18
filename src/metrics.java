f237fa31dba8b3c330c8bfb08f18120a64a2592f
CKiyomura <ckiyomura@columba.xyz>
2018-10-18 15:29:57 -0700

    Recompiled metrics, lineCollect and operandCollect back into a single file

--- src/metrics.java
+++ src/metrics.java
@@ -8,6 +8,9 @@ accepts wild card file statements, parses simple flags and is able to
 count a source files number of comments or lines of code.
 ********************************************************************/
 import java.io.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class metrics
 {
@@ -304,5 +307,378 @@ public class metrics
         for (int j = 0; j < spareLength; j++) {
             System.out.print(" ");
         }
+    }
+}
+
+class operandCollect
+{
+    final char[] HALF_OPERATOR = {'=', '+', '-', '*', '/', '%','!', '>', '<', '&', '|', '?', '~', '^', ':'};
+
+    final String[] FULL_OPERATOR = {"=", "==", "+", "++", "+=", "-", "--", "-=", "*", "*=", "/", "/=", "%", "%=",
+            "!", "!=", ">", ">=", ">>", ">>>", "<", "<=", "<<", "&", "&&", "&=", "|", "||", "|=", "?:", "~", "^", "^="};
+
+    private  char holder = '0';
+    private  char opCase = '0';
+    private  boolean operating = false;
+    int N1;
+    int N2;
+    int nOne;
+    int nTwo;
+    int programVocab;
+    int programLength;
+    double calcProgLength;
+    double volume;
+    double difficulty;
+    double effort;
+    double timeReq;
+    double bugs;
+    ArrayList<String> codeList = new ArrayList<>();
+    ArrayList<String> operatorList = new ArrayList<>();
+    ArrayList<String> operandList = new ArrayList<>();
+
+    public void parseOps(BufferedReader readFile)throws Exception
+    {
+        /*File currentFile = new File("NthPrime.java");
+        BufferedReader reader = new BufferedReader(new FileReader(currentFile));*/
+        StreamTokenizer streamIt = new StreamTokenizer(readFile);
+
+        streamIt.slashStarComments(true);
+        while(streamIt.nextToken() != streamIt.TT_EOF){
+            if(streamIt.sval != null || streamIt.ttype == StreamTokenizer.TT_NUMBER) {
+                if(holder == '>' && opCase == '>'){
+                    codeList.add(">>");
+                    reset();
+                } else if (holder != '0' && holder != '?') {
+                    codeList.add("" + holder);
+                    reset();
+                }
+            }
+            if (streamIt.sval != null) {
+                codeList.add(streamIt.sval);
+                operating = false;
+            } else if (streamIt.ttype == StreamTokenizer.TT_NUMBER) {
+                codeList.add("" + streamIt.nval);
+                operating = false;
+            } else {
+                if (holder == '>' && opCase == '>') {
+                    if((char)streamIt.ttype == '>') {
+                        codeList.add(">>>");
+                    } else if((char)streamIt.ttype == '=') {
+                        codeList.add(">>=");
+                    }
+                    reset();
+                } else if (!operating) {
+                    for (char c : HALF_OPERATOR) {
+                        if (c != ':') {
+                            if (c == (char) streamIt.ttype) {
+                                holder = (char) streamIt.ttype;
+                                operating = true;
+                            }
+                        }
+                    }
+                } else {
+                    for (char c : HALF_OPERATOR) {
+                        if (c == (char) streamIt.ttype) {
+                            opCase = (char) streamIt.ttype;
+                        }
+                    }
+                }
+            }
+            if(operating && opCase != '0') {
+                switch (holder){
+                    case '=':
+                        if(opCase == '=')
+                            codeList.add("==");
+                        reset();
+                        break;
+
+                    case '+':
+                        if(opCase == '+')
+                            codeList.add("++");
+                        else if(opCase == '=')
+                            codeList.add("+=");
+                        reset();
+                        break;
+
+                    case '-':
+                        if(opCase == '-')
+                            codeList.add("--");
+                        else if(opCase == '=')
+                            codeList.add("-=");
+                        reset();
+                        break;
+
+                    case '*':
+                        if(opCase == '=')
+                            codeList.add("*=");
+                        reset();
+                        break;
+
+                    case '/':
+                        if(opCase == '=')
+                            codeList.add("/=");
+                        reset();
+                        break;
+
+                    case '%':
+                        if(opCase == '=')
+                            codeList.add("%=");
+                        reset();
+                        break;
+
+                    case '!':
+                        if(opCase == '=')
+                            codeList.add("!=");
+                        reset();
+                        break;
+
+                    case '>':
+                        if(opCase == '=')
+                            codeList.add(">=");
+                        else if(opCase == '>')
+                            break;
+                        reset();
+                        break;
+
+                    case '<':
+                        if(opCase == '=')
+                            codeList.add("<=");
+                        else if(opCase == '<')
+                            codeList.add("<<");
+                        reset();
+                        break;
+
+                    case '&':
+                        if(opCase == '&')
+                            codeList.add("&&");
+                        else if(opCase == '=')
+                            codeList.add("&=");
+                        reset();
+                        break;
+
+                    case '|':
+                        if(opCase == '|')
+                            codeList.add("||");
+                        else if(opCase == '=')
+                            codeList.add("|=");
+                        reset();
+                        break;
+
+                    case '?':
+                        if(opCase == ':')
+                            codeList.add("?:");
+                        reset();
+                        break;
+
+                    case '^':
+                        if(opCase == '=')
+                            codeList.add("^=");
+                        reset();
+                        break;
+
+                    default:
+                        reset();
+                        break;
+                }
+            }
+        }
+    }
+
+    public void countN()
+    {
+        int operandIndex = -1;
+
+        for (int i = 0; i < this.codeList.size(); i++) {
+            for (String s : FULL_OPERATOR) {
+                if(this.codeList.get(i).equals(s)){
+                    if(this.codeList.get(i).equals("~")){                                               //~ is in front of operand
+                        operatorList.add(this.codeList.get(i));
+                        operandList.add(this.codeList.get(i+1));
+                        operandIndex = i+1;
+                    } else if(this.codeList.get(i).equals("++") || this.codeList.get(i).equals("--")){  //behind operand
+                        operatorList.add(this.codeList.get(i));
+                        if(i-1 != operandIndex)
+                            operandList.add(this.codeList.get(i-1));
+                    } else {                                                                            //operands on either side
+                        operatorList.add(this.codeList.get(i));
+                        operandList.add(this.codeList.get(i+1));
+                        if(i-1 != operandIndex)
+                            operandList.add(this.codeList.get(i-1));
+                        operandIndex = i+1;
+                    }
+                }
+            }
+        }
+        N1 = operatorList.size();
+        N2 = operandList.size();
+        Set<String> uniqueOperator = new HashSet<>(operatorList);
+        Set<String> uniqueOperand = new HashSet<>(operandList);
+        nOne = uniqueOperator.size();
+        nTwo = uniqueOperand.size();
+        programVocab = nOne + nTwo;
+        programLength = N1 + N2;
+        calcProgLength = nOne * (Math.log(nOne)/Math.log(2)) + nTwo * (Math.log(nTwo)/Math.log(2));
+        volume = programLength * (Math.log(programVocab)/Math.log(2));
+        difficulty = (nOne/2)*(N2/nTwo);
+        effort = difficulty * volume;
+        timeReq = effort/18;
+        bugs = volume/3000;
+    }
+
+    private void reset(){
+        holder = '0';
+        opCase = '0';
+        operating = false;
+    }
+}
+
+class lineCollect {
+    //This code has been acquired from https://gist.github.com/shiva27/143229. I have extended their javadoc code
+    //to include both an @author and an @see to the page of this code. This code has been slightly modified
+    //to fit my needs.
+    /**
+     * @author shiva27
+     * @see <a href="https://gist.github.com/shiva27/1432290">https://gist.github.com/shiva27/1432290</a>
+     */
+    public static int[] getNumberOfLines(BufferedReader bReader)
+            throws IOException {
+        int countSource = 0;
+        int countComment = 0;
+        boolean commentBegan = false;
+        String line = null;
+
+        while ((line = bReader.readLine()) != null) {
+            line = line.trim();
+            if ("".equals(line)) {
+                continue;
+            }
+            if (line.startsWith("//")) {
+                countComment++;
+                continue;
+            }
+            if (commentBegan) {
+                countComment++;
+                if (commentEnded(line)) {
+                    line = line.substring(line.indexOf("*/") + 2).trim();
+                    commentBegan = false;
+                    if ("".equals(line)) {
+                        continue;
+                    }
+                    if (line.startsWith("//")) {
+                        continue;
+                    }
+                } else {
+                    continue;
+                }
+            }
+            if (isSourceCodeLine(line)) {
+                countSource++;
+            }
+            if (commentBegan(line)) {
+                commentBegan = true;
+            }
+        }
+        return new int[] {countSource, countComment};
+    }
+
+    /**
+     * @author shiva27
+     * @see <a href="https://gist.github.com/shiva27/1432290">https://gist.github.com/shiva27/1432290</a>
+     * @param line
+     * @return This method checks if in the given line a comment has begun and has not ended
+     */
+    private static boolean commentBegan(String line) {
+        // If line = /* */, this method will return false
+        // If line = /* */ /*, this method will return true
+        int index = line.indexOf("/*");
+        if (index < 0) {
+            return false;
+        }
+        int quoteStartIndex = line.indexOf("\"");
+        if (quoteStartIndex != -1 && quoteStartIndex < index) {
+            while (quoteStartIndex > -1) {
+                line = line.substring(quoteStartIndex + 1);
+                int quoteEndIndex = line.indexOf("\"");
+                line = line.substring(quoteEndIndex + 1);
+                quoteStartIndex = line.indexOf("\"");
+            }
+            return commentBegan(line);
+        }
+        return !commentEnded(line.substring(index + 2));
+    }
+
+    /**
+     * @author shiva27
+     * @see <a href="https://gist.github.com/shiva27/1432290">https://gist.github.com/shiva27/1432290</a>
+     * @param line
+     * @return This method checks if in the given line a comment has ended and no new comment has not begun
+     */
+    private static boolean commentEnded(String line) {
+        // If line = */ /* , this method will return false
+        // If line = */ /* */, this method will return true
+        int index = line.indexOf("*/");
+        if (index < 0) {
+            return false;
+        } else {
+            String subString = line.substring(index + 2).trim();
+            if ("".equals(subString) || subString.startsWith("//")) {
+                return true;
+            }
+            if(commentBegan(subString))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+
+    /**
+     * @author shiva27
+     * @see <a href="https://gist.github.com/shiva27/1432290">https://gist.github.com/shiva27/1432290</a>
+     * @param line
+     * @return This method returns true if there is any valid source code in the given input line. It does not worry if comment has begun or not.
+     * This method will work only if we are sure that comment has not already begun previously. Hence, this method should be called only after {@link #commentBegan(String)} is called
+     */
+    private static boolean isSourceCodeLine(String line) {
+        boolean isSourceCodeLine = false;
+        line = line.trim();
+        if ("".equals(line) || line.startsWith("//")) {
+            return isSourceCodeLine;
+        }
+        if (line.length() == 1) {
+            return true;
+        }
+        int index = line.indexOf("/*");
+        if (index != 0) {
+            return true;
+        } else {
+            while (line.length() > 0) {
+                line = line.substring(index + 2);
+                int endCommentPosition = line.indexOf("*/");
+                if (endCommentPosition < 0) {
+                    return false;
+                }
+                if (endCommentPosition == line.length() - 2) {
+                    return false;
+                } else {
+                    String subString = line.substring(endCommentPosition + 2)
+                            .trim();
+                    if ("".equals(subString) || subString.indexOf("//") == 0) {
+                        return false;
+                    } else {
+                        if (subString.startsWith("/*")) {
+                            line = subString;
+                            continue;
+                        }
+                        return true;
+                    }
+                }
+
+            }
+        }
+        return isSourceCodeLine;
     }
 }
